0c8e31ff2fd619b107168980971833be894ef4c7
VovaZhelezkov <vovikzhelezkov@gmail.com>
2020-07-29 22:00:39 +0300

    change

--- srv.py
+++ srv.py
@@ -1,3 +1,17 @@
 import os
+import socketserver
+from http.server import SimpleHTTPRequestHandler
 
+PORT = int(os.getenv("PORT", 8000))
+print(PORT)
+
+
+class MyHandler(SimpleHTTPRequestHandler):
+    pass
+
+
+if __name__ == "__main__":
+    with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+        print("it" + " works")
+        httpd.serve_forever(poll_interval=1)
 
