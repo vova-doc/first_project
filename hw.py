b5978f6047de3fed51c1711ebb99aa9d9a126bba
VovaZhelezkov <vovikzhelezkov@gmail.com>
2020-07-29 20:49:55 +0300

    pypy

--- hw.py
+++ hw.py
@@ -1,5 +1,15 @@
-import django as x
 
-x = x.__version__
-print (f"django version: {x} - type {type(x)}")
 
+x = 1
+
+def f1():
+    x = 2
+    print(x)
+
+def f2():
+    x = 3
+    print(x)
+
+f1()
+f2()
+print(x)
