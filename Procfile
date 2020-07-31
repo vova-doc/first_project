13c7febb3f9bb582f9b5efd05c1b341b67cfd8ed
VovaZhelezkov <vovikzhelezkov@gmail.com>
2020-07-31 20:12:36 +0300

    change

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: python -m http.server $PORT
+web: python3 srv.py
 
--- srv.py
+++ srv.py
@@ -2,12 +2,25 @@ import os
 import socketserver
 from http.server import SimpleHTTPRequestHandler
 
+
 PORT = int(os.getenv("PORT", 8000))
 print(PORT)
 
 
 class MyHandler(SimpleHTTPRequestHandler):
-    pass
+    def do_GET(self):
+        content = """
+        <html>
+        <head><title>XXX</title></head>
+        <body>Hello World!</body>
+        </html>
+        """
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-leght", str(len(content)))
+        self.end_headers()
+        self.wfile.write(content.encode())
 
 
 if __name__ == "__main__":
