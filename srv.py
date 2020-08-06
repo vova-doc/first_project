9ca1bfe1c948f381c93131c3cc913fb3eb407d86
VovaZhelezkov <vovikzhelezkov@gmail.com>
2020-08-06 21:56:26 +0300

    change

--- srv.py
+++ srv.py
@@ -56,7 +56,7 @@ class MyHandler(SimpleHTTPRequestHandler):
 
 
 if __name__ == "__main__":
-    with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+    with socketserver.TCPServer(("", settings.PORT), MyHandler) as httpd:
         print("it" + " works")
         httpd.serve_forever(poll_interval=1)
 
