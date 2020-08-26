2afdb7863db568183a9c5ccdf3813ad291b9a8c0
VovaZhelezkov <vovikzhelezkov@gmail.com>
2020-08-26 18:03:51 +0300

    change

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import socketserver
 import settings
 
-from srv.py import MyHttp
+from srv import MyHttp
 
 if __name__ == "__main__":
     with socketserver.TCPServer(("", settings.PORT), MyHttp) as httpd:
